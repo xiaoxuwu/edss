7280426af2ed335018f2c93841a7190cee63323d
Alex Hunsader <alex@hunsader.com>
2018-08-14 19:18:43 -0700

    changed media root, added student api to url patterns

--- student_showcase/web/urls.py
+++ student_showcase/web/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from rest_framework import routers
 import api.views as api_view
@@ -21,10 +23,11 @@ from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'company', api_view.CompanyViewSet)
+//router.register(r'student', api_view.StudentViewSet)
 
 urlpatterns = [
     url(r'^$', api_view.api_root),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(router.urls)),
     url(r'^auth/', views.obtain_auth_token)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
